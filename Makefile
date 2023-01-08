25a174b0d208baddbb7e9575e0a098428578b12e
Kevin Goldsmith <kevin@unitcircle.com>
2023-01-08 02:35:16 -0800

    black on parse_calendar nice Makefile rule for lint

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
-lint:
-	pylint *.py
+lint: ; @for py in *.py; do echo "Linting $$py"; pylint -rn $$py; done
 
 black:
 	black .
 
 mypy:
 	mypy --disallow-untyped-defs .
+
+list:
+	@grep '^[^#[:space:]].*:' Makefile
 
 all: lint black mypy
--- parse_calendar.py
+++ parse_calendar.py
@@ -6,13 +6,13 @@ from icalendar import Calendar
 import recurring_ical_events
 import x_wr_timezone
 
-_DEFAULT_DATA_DIR = 'data'
-_CONFIG_FILE = 'config.ini'
+_DEFAULT_DATA_DIR = "data"
+_CONFIG_FILE = "config.ini"
 
 _config_parser = None
 _console_log_level = logging.INFO
 _logfile_log_level = logging.INFO
-_logfile_name = 'parse_calendar.log'
+_logfile_name = "parse_calendar.log"
 _logger = logging.getLogger(__name__)
 
 
@@ -21,11 +21,11 @@ def load_config_file() -> None:
     _config_parser = configparser.ConfigParser()
     _config_parser.read(_CONFIG_FILE)
     # set variables from config
-    if 'logging' in _config_parser:
-        logging_config = _config_parser['logging']
-        _console_log_level = logging_config.get('console_log_level', _console_log_level)
-        _logfile_log_level = logging_config.get('logfile_log_level', _logfile_log_level)
-        _logfile_name = logging_config.get('logfile_name', _logfile_name)
+    if "logging" in _config_parser:
+        logging_config = _config_parser["logging"]
+        _console_log_level = logging_config.get("console_log_level", _console_log_level)
+        _logfile_log_level = logging_config.get("logfile_log_level", _logfile_log_level)
+        _logfile_name = logging_config.get("logfile_name", _logfile_name)
 
 
 def update_config_file() -> None:
@@ -36,14 +36,14 @@ def update_config_file() -> None:
         _config_parser = configparser.ConfigParser()
         # raise RuntimeError("update_config_file called before load_config_file")
     # config_parser['Login Parameters']['refresh_token'] = token_dict['refresh_token']
-    with open(_CONFIG_FILE, 'w') as configfile:
+    with open(_CONFIG_FILE, "w") as configfile:
         _config_parser.write(configfile)
 
 
 def initialize_logging() -> None:
     _logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(name)s - %(asctime)s (%(levelname)s): %(message)s')
-    formatter.datefmt = '%Y-%m-%d %H:%M:%S %z'
+    formatter = logging.Formatter("%(name)s - %(asctime)s (%(levelname)s): %(message)s")
+    formatter.datefmt = "%Y-%m-%d %H:%M:%S %z"
     ch = logging.StreamHandler()
     ch.setLevel(_console_log_level)
     ch.setFormatter(formatter)
@@ -56,23 +56,23 @@ def initialize_logging() -> None:
 
 def main() -> None:
     start_date = (2023, 1, 1)
-    end_date =   (2023, 1, 31)
+    end_date = (2023, 1, 31)
 
-    with open("distrokid.ics", 'rb') as file:
+    with open("distrokid.ics", "rb") as file:
         calendar = Calendar.from_ical(file.read())
     new_calendar = x_wr_timezone.to_standard(calendar)
     events = recurring_ical_events.of(new_calendar).between(start_date, end_date)
 
     for event in events:
-        print(event['summary'])
+        print(event["summary"])
         print(f"\t{event['dtstart'].dt} - {event['dtend'].dt}")
-        if 'description' in event:
-            print('\t' + event['description'])
+        if "description" in event:
+            print("\t" + event["description"])
         print(f"\ttimestamp:{event['dtstamp'].dt}")
-        if 'attendee' in event:
-            for attendee in event['attendee']:
+        if "attendee" in event:
+            for attendee in event["attendee"]:
                 print(f"\tattendee: {attendee}")
-        if 'organizer' in event:
+        if "organizer" in event:
             print(f"\torganizer: {event['organizer']}")
         print(f"\t{event['status']}")
 
@@ -91,19 +91,21 @@ def main() -> None:
     """
 
 
-#when run as a script, do initialization
+# when run as a script, do initialization
 if __name__ == "__main__":
     load_config_file()
 
     # command-line arguments override config file settings
-    parser = argparse.ArgumentParser(description='Convert calendar file into json event and people list.')
-    parser.add_argument('--verbose', '-v', action='store_true', dest='verbose')
-    parser.add_argument('--verbose_log', '-V', action='store_true', dest='verbose_log')
+    parser = argparse.ArgumentParser(
+        description="Convert calendar file into json event and people list."
+    )
+    parser.add_argument("--verbose", "-v", action="store_true", dest="verbose")
+    parser.add_argument("--verbose_log", "-V", action="store_true", dest="verbose_log")
     ns = parser.parse_args()
     if ns.verbose:
         _console_log_level = logging.DEBUG
     if ns.verbose_log:
         _logfile_log_level = logging.DEBUG
-    
+
     initialize_logging()
     main()
