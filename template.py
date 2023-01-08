d5c38286e4e3ad183a72a9754ba5d1f62e13fc9b
Kevin Goldsmith <kevin@unitcircle.com>
2023-01-07 23:02:04 -0800

    added etlutils to requirements added initialization to parse_calendar and cleaned up code some .gitignore adds created new template.py file for use in future projects

--- template.py
+++ template.py
@@ -2,11 +2,7 @@ import argparse
 import configparser
 import logging
 
-from icalendar import Calendar
-import recurring_ical_events
-import x_wr_timezone
 
-_DEFAULT_DATA_DIR = 'data'
 _CONFIG_FILE = 'config.ini'
 
 _config_parser = None
@@ -55,48 +51,14 @@ def initialize_logging() -> None:
 
 
 def main() -> None:
-    start_date = (2023, 1, 1)
-    end_date =   (2023, 1, 31)
-
-    with open("distrokid.ics", 'rb') as file:
-        calendar = Calendar.from_ical(file.read())
-    new_calendar = x_wr_timezone.to_standard(calendar)
-    events = recurring_ical_events.of(new_calendar).between(start_date, end_date)
-
-    for event in events:
-        print(event['summary'])
-        print(f"\t{event['dtstart'].dt} - {event['dtend'].dt}")
-        if 'description' in event:
-            print('\t' + event['description'])
-        print(f"\ttimestamp:{event['dtstamp'].dt}")
-        if 'attendee' in event:
-            for attendee in event['attendee']:
-                print(f"\tattendee: {attendee}")
-        if 'organizer' in event:
-            print(f"\torganizer: {event['organizer']}")
-        print(f"\t{event['status']}")
-
-    """
-    for component in new_calendar.walk():
-        if component.name == "VEVENT":
-            print(component.get('summary'))
-            print(f"\t{component.get('dtstart').dt} - {component.get('dtend').dt}")
-            print(component.get('description'))
-            print(f"\ttimestamp:{component.get('dtstamp').dt}")
-            if component.get('attendee'):
-                for attendee in component.get('attendee'):
-                    print(f"\tattendee: {attendee}")
-            print(f"\torganizer: {component.get('organizer')}")
-            print(f"\t{component.get('status')}")
-    """
-
+    pass
 
 #when run as a script, do initialization
 if __name__ == "__main__":
     load_config_file()
 
     # command-line arguments override config file settings
-    parser = argparse.ArgumentParser(description='Convert calendar file into json event and people list.')
+    parser = argparse.ArgumentParser(description='do something interesting.')
     parser.add_argument('--verbose', '-v', action='store_true', dest='verbose')
     parser.add_argument('--verbose_log', '-V', action='store_true', dest='verbose_log')
     ns = parser.parse_args()
